cc30035b2c2ac58dcadd78ec2091971984a08396
2025499 <2025499@student.cct.ie>
2026-05-15 13:52:12 +0100

    Implement EmployeeNode class for binary tree structure

--- src/main/java/com/mycompany/ca_2/EmployeeNode.java
+++ src/main/java/com/mycompany/ca_2/EmployeeNode.java
@@ -8,6 +8,19 @@ package com.mycompany.ca_2;
  *
  * @author wanghuohuo
  */
+
+// Node class for binary tree
 public class EmployeeNode {
     
+    Employee data;
+    EmployeeNode left;
+    EmployeeNode right;
+
+    // Constructor: create a node with employee data
+    
+    public EmployeeNode(Employee data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
