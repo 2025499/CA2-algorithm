7135a65e5348f35d6e145467b7a3b3c0711c4cb6
2025499 <2025499@student.cct.ie>
2026-05-15 12:54:26 +0100

    Creat MenuOption enum for system menu choices

---
+++ src/main/java/com/mycompany/ca_2/MenuOption.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ca_2;
+
+/**
+ *
+ * @author wanghuohuo
+ */
+public enum MenuOption {
+   
+    SORT,
+    SEARCH,
+    ADD_EMPLOYEE,
+    BUILD_TREE,
+    EXIT
+}
