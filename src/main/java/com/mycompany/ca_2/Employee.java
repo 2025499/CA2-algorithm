4834ed43f7168a222d76587dc3a76bfe959ddd19
2025499 <2025499@student.cct.ie>
2026-05-15 12:33:57 +0100

    Create Employee class using Department and Manager objects

--- src/main/java/com/mycompany/ca_2/Employee.java
+++ src/main/java/com/mycompany/ca_2/Employee.java
@@ -8,6 +8,40 @@ package com.mycompany.ca_2;
  *
  * @author wanghuohuo
  */
+
+// Employee class: stores all employee details
 public class Employee {
+    
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String managerType;
+
+    // Constructor: create a new employee
+    
+    public Employee(String firstName, String lastName, String department, String managerType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.managerType = managerType;
+    }
+
+    // Get full name
+    
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    // Get department
+    
+    public String getDepartment() {
+        return department;
+    }
+
+    // Get manager type
+   
+    public String getManagerType() {
+        return managerType;
+    }
     
 }
