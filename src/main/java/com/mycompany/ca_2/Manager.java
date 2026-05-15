9956191949cf617132fd6c7dc48daadb57f3bf20
2025499 <2025499@student.cct.ie>
2026-05-15 11:41:37 +0100

    Complete Manager class with attributes and getters

--- src/main/java/com/mycompany/ca_2/Manager.java
+++ src/main/java/com/mycompany/ca_2/Manager.java
@@ -8,6 +8,24 @@ package com.mycompany.ca_2;
  *
  * @author wanghuohuo
  */
+
+// Manager class: stores the type of manager
 public class Manager {
+    
+    private String type;
+
+    // Constructor: create a manager type
+  
+    public Manager(String type) {
+        this.type = type;
+    }
+
+    // Get manager type
+   
+    public String getType() {
+        return type;
+    }
+    
+    
     
 }
